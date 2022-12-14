be06666103415b553766b8856e91497cb51e5a18
drsevin <sevindere@gmail.com>
2022-12-14 21:30:47 +0300

    first commit

---
+++ WebTatilSitesi/WebTatilSitesi/Controllers/AnaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebTatilSitesi.Controllers
+{
+    public class AnaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
